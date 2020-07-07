28e6aa474a2f47e4be2a273b189e10a83c296e1b
VassilisDaioglou <42271684+VassilisDaioglou@users.noreply.github.com>
2020-07-07 12:29:03 +0200

    Cleaned up and made consistent the datasets to be used in the supplementary data

--- BuildStocks.R
+++ BuildStocks.R
@@ -472,31 +472,65 @@ rm(temp,temp1,temp.Base,temp.EffNew,temp.EffNewRen,temp.Tot,Area.EffNew,Area.Eff
 
 # 
 # ---- DATASET FOR SUPPLEMENTARY DATA ----
+ScenarioDF <- data.frame(Scenario = c("SSP1_Baseline",
+                                         "SSP2_Basline",
+                                         "SSP3_Baseline",
+                                         "SSP4_Baseline",
+                                         "SSP5_Baseline",
+                                         "SSP2_450_Baseline",
+                                         "SSP2_450_InsulAll",
+                                         "SSP2_450_InsulNew"),
+                           ScenLabel = c("SSP1",
+                                          "SSP2",
+                                          "SSP3",
+                                          "SSP4",
+                                          "SSP5",
+                                          "2°C",
+                                          "NewInsul",
+                                          "AllInsul"))
+
 SupData.FE = rbind(DATA.FE, DATA.PV)
 SupData.FE = subset(SupData.FE, Variable=="FEHeat"|Variable=="FECool"|Variable=="FEResGenerationElec")
 SupData.FE = subset(SupData.FE, select = -c(Scenario,Prim))
-SupData.FE = SupData.FE[,c(5,1,2,3,7,4,6)]
+SupData.FE$ScenName = ScenarioDF[match(SupData.FE$ScenOrder,ScenarioDF$Scenario),"ScenLabel"]
+SupData.FE$ScenOrder <- NULL
+SupData.FE = SupData.FE[,c(7,1,2,3,6,4,5)]
 colnames(SupData.FE)[] <- c("Scenario","Region","Year","Variable","Energy Carrier","Unit","Value")
+SupData.FE$Variable = gsub("FEHeat","Heating",SupData.FE$Variable)
+SupData.FE$Variable = gsub("FECool","Cooling",SupData.FE$Variable)
+SupData.FE$Variable = gsub("FEResGenerationElec","Rooftop PV Generation",SupData.FE$Variable)
 
 SupData.FS = DATA.FS
 SupData.FS = subset(SupData.FS, select = -c(Scenario))
+SupData.FS$ScenName = ScenarioDF[match(SupData.FS$ScenOrder,ScenarioDF$Scenario),"ScenLabel"]
+SupData.FS$ScenOrder <- NULL
 SupData.FS = SupData.FS[,c(7,1,2,3,4,5,6)]
 colnames(SupData.FS)[] <- c("Scenario","Region","Year","Variable","Insulation Level","Unit","Value")
 
 SupData.UE = subset(DATA.UE, Variable == "UEHeatCool")
 SupData.UE = subset(SupData.UE, select = -c(Scenario, Normalised_2020))
-SupData.UE = SupData.UE[,c(4,5,3,1,2,6)]
+SupData.UE$ScenName = ScenarioDF[match(SupData.UE$ScenOrder,ScenarioDF$Scenario),"ScenLabel"]
+SupData.UE$ScenOrder <- NULL
+SupData.UE = SupData.UE[,c(6,4,3,1,2,5)]
 colnames(SupData.UE)[] <- c("Scenario","Region","Year","Variable","Unit","Value")
+SupData.UE$Variable = gsub("UEHeatCool","Heating & Cooling Useful Energy Demand",SupData.UE$Variable)
 
 SupData.EM = subset(DATA.EM, Variable == "EmisCO2DirectHeatCool"|Variable=="EmisCO2HeatCool")
 SupData.EM = subset(SupData.EM, select = -c(Scenario))
-SupData.EM = SupData.EM[,c(4,5,3,1,2,6)]
+SupData.EM$ScenName = ScenarioDF[match(SupData.EM$ScenOrder,ScenarioDF$Scenario),"ScenLabel"]
+SupData.EM$ScenOrder <- NULL
+SupData.EM = SupData.EM[,c(6,4,3,1,2,5)]
 colnames(SupData.EM)[] <- c("Scenario","Region","Year","Variable","Unit","Value")
+SupData.EM$Variable = gsub("EmisCO2DirectHeatCool","Heating & Cooling Direct Emissions",SupData.EM$Variable)
+SupData.EM$Variable = gsub("EmisCO2HeatCool","Heating & Cooling Direct & Indirect Emissions",SupData.EM$Variable)
 
 SupData.RR = subset(DATA.RR, Year %in% ActiveYears2)
 SupData.RR = subset(SupData.RR, select = -c(Scenario))
-SupData.RR = SupData.RR[,c(4,5,3,1,2,6)]
+SupData.RR$ScenName = ScenarioDF[match(SupData.RR$ScenOrder,ScenarioDF$Scenario),"ScenLabel"]
+SupData.RR$ScenOrder <- NULL
+SupData.RR = SupData.RR[,c(6,4,3,1,2,5)]
 colnames(SupData.RR)[] <- c("Scenario","Region","Year","Variable","Unit","Value")
+SupData.RR$Variable = gsub("InsulAverageRenovRate","Average Renovation rate since 2020",SupData.RR$Variable)
 
 
 #

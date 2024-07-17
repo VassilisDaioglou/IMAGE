e86dc74646931d824b01aefde2e43325f7abb801
VassilisDaioglou <vassilis.daioglou@pbl.nl>
2024-07-17 14:51:38 +0200

    Added scripts that make variables lists based on common-definitions. Can work with a variable template as well

---
+++ script/make_complete_variable_list.py
@@ -0,0 +1,59 @@
+'''
+This script makes a complete list of variables, units and definitions
+based on what is available on the common-definitions repository
+https://github.com/IAMconsortium/common-definitions
+
+It is a concise version of the make_variable_list.py script which has added functionaliy
+- Get limited variable list based on a template
+- Get limited variable definitions based on a template
+
+'''
+
+from nomenclature import DataStructureDefinition
+from nomenclature.codelist import VariableCodeList
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+print("\nSet Definitions")
+definitions_project = "common-definitions"
+working_project = "IMAGE34"
+
+# The working-directory of the Python console has to be set to the clone of the project repository.
+definitions_directory = str('C:\\Users\\daioglouv\\Documents\\git\\' + definitions_project)
+output_directory = str('C:\\Users\\daioglouv\\Documents\\git\\IMAGE\\output\\' + working_project + '\\')
+Path(output_directory).mkdir(parents=True, exist_ok=True)
+
+print("\nLoad Functions")
+def get_complete_variable_list():
+    '''
+    # Get complete list 
+    - To be used when all variable definitions are to be used
+    '''
+    dsd = DataStructureDefinition(definitions_directory + "/definitions/")
+    df = dsd.variable.to_pandas()
+    df = df[['Variable','Unit','Description']]
+    return df
+
+'''
+ ********** STARTING PROCEDURE **********
+'''
+print("\nGetting Variable List & Definitions")
+complete_variable_list = get_complete_variable_list()
+
+print("\nProduce output")
+# Write output excel
+time = datetime.now()
+timestamp = time.strftime("%d%m%Y_%H%M")
+data_file = str(definitions_project + '_variable_list_' + timestamp + '.xlsx')
+
+print("\ttimestamp:       ", timestamp)
+print("\tOutput Location: ", output_directory)
+print("\tOutput File:     ", data_file)
+
+writer = pd.ExcelWriter(output_directory + data_file, engine = 'xlsxwriter')
+complete_variable_list.to_excel(writer, sheet_name= 'variable_list', index=False)
+writer.close()
+
+print("\nDone!")
---
+++ script/make_variable_list.py
@@ -0,0 +1,89 @@
+from nomenclature import DataStructureDefinition
+from nomenclature.codelist import VariableCodeList
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import os
+
+definitions_project = "common-definitions"
+working_project = "OECD_EO_2025"
+data_file = "Consolidated_IMAGE_OECD_EO_2025_results_05092024.xlsx"
+
+# The working-directory of the Python console has to be set to the clone of the project repository.
+definitions_directory = str('C:\\Users\\daioglouv\\Documents\\git\\' + definitions_project)
+#template_directory = str('C:\\Users\\daioglouv\\Documents\\git\\IMAGE\\data\\' + working_project + '\\' + working_project + '_template.xlsx')
+template_directory = str('C:\\Users\\daioglouv\\Documents\\git\\IMAGE\\data\\' + working_project + '\\' + data_file)
+output_directory = str('C:\\Users\\daioglouv\\Documents\\git\\IMAGE\\output\\' + working_project + '\\')
+
+print("Load Functions")
+
+def get_limited_variable_list(variable_group):
+    '''
+    Get variables from specific sub-folder
+        - To be used when only a specific sub-folder of the definitions is used
+    '''
+    path = Path(definitions_directory + 'definitions/variable/' + variable_group)
+    codelist = VariableCodeList.from_directory(path=path, name='Variable')
+    df = codelist.to_pandas()
+    df = df[['Variable','Unit','Description']]
+    return df
+
+def get_complete_variable_list():
+    '''
+    # Get complete list 
+    - To be used when all variable definitions are to be used
+    '''
+    dsd = DataStructureDefinition(definitions_directory + "/definitions/")
+    df = dsd.variable.to_pandas()
+    df = df[['Variable','Unit','Description']]
+    return df
+
+def get_template_description(template, variable_unit_description):
+    '''
+    inputs: 
+        - template with variables to be reported
+        - variable_unit_description contains full list of variables
+
+        Both as pandas dataframes
+    
+    outputs:
+        - Dataframe with template variables together with definitions
+        - Dataframe with template variables that do not have definitions
+    '''
+    template_array = np.array(template.Variable)
+    
+    template_variable_description   = variable_unit_description[variable_unit_description['Variable'].isin(template_array)]
+
+    # Identify template Variables that do not have a description
+    missing = []
+    for v in template_array:
+        if v not in np.array(template_variable_description.Variable):
+            missing.append(v)
+    
+    missing_df = pd.DataFrame(missing, columns=['Variable'])
+    missing_df = missing_df.drop_duplicates()
+
+    return template_variable_description, missing_df
+
+'''
+ ********** STARTING PROCEDURE **********
+'''
+print("Getting Variable Definitions")
+# Load Variable Lists to assign definitions to
+reported_data = pd.read_excel(template_directory, sheet_name='data')
+reported_missing = pd.read_excel(template_directory, sheet_name='variables_missing')
+
+# Get all variable and definition data frames
+complete_variable_list = get_complete_variable_list()
+vars_with_description, vars_missing_description = get_template_description(reported_data, complete_variable_list)
+
+print("Produce output")
+# Write output excel
+writer = pd.ExcelWriter(output_directory + data_file, engine = 'xlsxwriter')
+reported_data.to_excel(writer, sheet_name= 'data', index=False)
+reported_missing.to_excel(writer, sheet_name= 'variables_missing', index=False)
+vars_with_description.to_excel(writer, sheet_name = 'variable_definitions', index = False)
+vars_missing_description.to_excel(writer, sheet_name = 'no_definitions', index = False)
+writer.close()
+
+print("Done!")
